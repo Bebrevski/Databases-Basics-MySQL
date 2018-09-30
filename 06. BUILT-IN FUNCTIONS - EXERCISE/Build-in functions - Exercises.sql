569c54234235cea4cc6c06229401501087c82be5
Bebrevski <stefan.bebrevski89@gmail.com>
2018-09-30 15:32:04 +0300

    Course exercises

--- 06. BUILT-IN FUNCTIONS - EXERCISE/Build-in functions - Exercises.sql
+++ 06. BUILT-IN FUNCTIONS - EXERCISE/Build-in functions - Exercises.sql
@@ -45,3 +45,85 @@ WHERE `name` REGEXP '^[^RBDrbd]'
 ORDER BY `name`;
 
 #-- 8.	Create View Employees Hired After 2000 Year
+CREATE VIEW `v_employees_hired_after_2000` AS
+	SELECT `first_name`, `last_name`
+    FROM `employees`
+    WHERE YEAR(`hire_date`) > 2000;
+    
+SELECT * FROM `v_employees_hired_after_2000`;
+
+#-- 9.	Length of Last Name
+SELECT `first_name`, `last_name`
+FROM `employees`
+WHERE CHAR_LENGTH(`last_name`) = 5;
+
+#-- 10.	Countries Holding ‘A’ 3 or More Times
+USE `geography`;
+
+SELECT `country_name`, `iso_code`
+FROM `countries`
+WHERE (CHAR_LENGTH(`country_name`) - CHAR_LENGTH(REPLACE(LOWER(`country_name`), 'a', ''))) >= 3
+ORDER BY `iso_code`;
+
+#-- 11.	 Mix of Peak and River Names
+SELECT 
+    `peak_name`,
+    `river_name`,
+    CONCAT('',
+            LOWER(`peak_name`),
+            LOWER(SUBSTRING(`river_name`, 2))) AS 'mix'
+FROM
+    `peaks`,
+    `rivers`
+WHERE
+    LOWER(RIGHT(`peak_name`, 1)) = LOWER(LEFT(`river_name`, 1))
+ORDER BY `mix`;
+
+#-- 12.	Games from 2011 and 2012 year
+USE `diablo`;
+
+SELECT `name`, DATE_FORMAT(`start`, '%Y-%m-%d') AS 'start'
+FROM `games`
+WHERE YEAR(`start`) BETWEEN 2011 AND 2012
+ORDER BY `start`, `name`
+LIMIT 50;
+
+#-- 13.	 User Email Providers
+SELECT `user_name`, SUBSTRING_INDEX(`email`, '@', -1) AS 'Email Provider'
+FROM `users`
+ORDER BY `Email Provider`, `user_name`;
+
+#-- 14.	 Get Users with IP Address Like Pattern
+SELECT `user_name`, `ip_address`
+FROM `users`
+WHERE `ip_address` LIKE '___.1%.%.___'
+ORDER BY `user_name`;
+
+#-- 15.	 Show All Games with Duration and Part of the Day
+SELECT 
+    `name` AS 'game',
+    CASE
+        WHEN HOUR(`start`) BETWEEN 0 AND 11 THEN 'Morning'
+        WHEN HOUR(`start`) BETWEEN 12 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS 'Part of the Day',
+    CASE
+        WHEN `duration` <= 3 THEN 'Extra Short'
+        WHEN `duration` BETWEEN 4 AND 6 THEN 'Short'
+        WHEN `duration` BETWEEN 7 AND 10 THEN 'Long'
+        ELSE 'Extra Long'
+    END AS 'Duration'
+FROM
+    `games`
+ORDER BY `name`;
+
+#-- 16.	 Orders Table
+USE `orders`;
+
+SELECT 
+    `product_name`,
+    `order_date`,
+    DATE_ADD(`order_date`, INTERVAL 3 DAY) AS 'pay_due',
+    DATE_ADD(`order_date`, INTERVAL 1 MONTH) AS 'deliver_due'
+FROM
+    `orders`;
