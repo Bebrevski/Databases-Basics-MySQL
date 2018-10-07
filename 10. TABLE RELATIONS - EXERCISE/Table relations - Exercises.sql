e0ef331fd2689332fab029c98a94fe23148c31d4
Bebrevski <stefan.bebrevski89@gmail.com>
2018-10-07 21:37:19 +0300

    Course exercises

--- 10. TABLE RELATIONS - EXERCISE/Table relations - Exercises.sql
+++ 10. TABLE RELATIONS - EXERCISE/Table relations - Exercises.sql
@@ -223,8 +223,63 @@ CREATE TABLE `order_items` (
 CREATE DATABASE `univercity_database`;
 USE `univercity_database`;
 
+CREATE TABLE `majors`(
+`major_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL
+);
 
+CREATE TABLE `students` (
+    `student_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `student_number` VARCHAR(12) NOT NULL,
+    `student_name` VARCHAR(50) NOT NULL,
+    `major_id` INT NOT NULL,
+    CONSTRAINT `fk_students_majors` 
+		FOREIGN KEY (`major_id`)
+        REFERENCES `majors` (`major_id`)
+);
 
+CREATE TABLE `payments` (
+    `payment_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `payment_date` DATE,
+    `payment_amount` DECIMAL(8 , 2 ),
+    `student_id` INT NOT NULL,
+    CONSTRAINT `fk_payments_students` 
+		FOREIGN KEY (`student_id`)
+        REFERENCES `students` (`student_id`)
+);
+
+CREATE TABLE `subjects` (
+    `subject_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `subject_name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `agenda` (
+    `student_id` INT NOT NULL,
+    `subject_id` INT NOT NULL,
+		CONSTRAINT `pk_agenda` 
+			PRIMARY KEY (`student_id` , `subject_id`),
+		CONSTRAINT `fk_agenda_students` 
+			FOREIGN KEY (`student_id`)
+			REFERENCES `students` (`student_id`),
+		CONSTRAINT `fk_agenda_subjects` 
+			FOREIGN KEY (`subject_id`)
+			REFERENCES `subjects` (`subject_id`)
+);
+
+#-- 9.	Peaks in Rila
+USE `geography`;
+
+SELECT 
+    m.mountain_range,
+    p.peak_name,
+    p.elevation AS 'peak_elevation'
+FROM
+    `mountains` AS m
+        JOIN
+    `peaks` AS p ON m.id = p.mountain_id
+WHERE
+    m.mountain_range = 'Rila'
+ORDER BY p.elevation DESC;
 
 
 
