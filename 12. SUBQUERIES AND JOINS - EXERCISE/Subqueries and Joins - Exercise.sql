c2461027ce9b0e501435022959c8c02925dbc1cb
Bebrevski <stefan.bebrevski89@gmail.com>
2018-10-11 20:45:03 +0300

    Course exercises

--- 12. SUBQUERIES AND JOINS - EXERCISE/Subqueries and Joins - Exercise.sql
+++ 12. SUBQUERIES AND JOINS - EXERCISE/Subqueries and Joins - Exercise.sql
@@ -29,8 +29,21 @@ ORDER BY first_name, last_name
 LIMIT 5;
 
 #-- 3.	Sales Employee
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    d.name AS 'department_name'
+FROM
+    employees AS e
+        JOIN
+    departments AS d 
+		ON e.department_id = d.department_id
+WHERE
+    d.name LIKE ('Sales')
+ORDER BY e.employee_id DESC;
 
-
+#-- 4.	Employee Departments
 
 
 
