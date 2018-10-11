e41d9a38d710b28ee9d6a2e1a6a3b135fe8c88a5
Bebrevski <stefan.bebrevski89@gmail.com>
2018-10-11 20:23:41 +0300

    Course exercises

--- 11. SUBQUERIES AND JOINS/Subqueries and Joins - Lab.sql
+++ 11. SUBQUERIES AND JOINS/Subqueries and Joins - Lab.sql
@@ -7,11 +7,41 @@ SELECT
     d.department_id,
     d.name AS department_id
 FROM
-    departments AS d
+    employees AS e
         INNER JOIN
-    employees AS e 
+    departments AS d 
 		ON e.employee_id = d.manager_id
 ORDER BY e.employee_id
 LIMIT 5;
 
 #-- 2.	Towns Adresses
+SELECT 
+    t.town_id, t.name AS town_name, a.address_text
+FROM
+    towns AS t
+        INNER JOIN
+    addresses AS a 
+		ON t.town_id = a.town_id
+WHERE t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY town_id, address_id;
+
+#-- 3.	Employees Without Managers
+SELECT 
+    employee_id, 
+    first_name, 
+    last_name, 
+    department_id, 
+    salary
+FROM
+    employees
+WHERE
+    manager_id IS NULL;
+
+#-- 4.	Higher Salary
+SELECT 
+    COUNT(employee_id) AS 'count'
+FROM
+    employees e
+WHERE
+    e.salary > (SELECT AVG(salary)
+				FROM employees);
