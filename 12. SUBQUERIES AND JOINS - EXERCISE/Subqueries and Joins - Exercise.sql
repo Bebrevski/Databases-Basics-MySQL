5446a4a3e064fae0fea89ef2e5551a29cb2808ff
Bebrevski <stefan.bebrevski89@gmail.com>
2018-10-13 16:50:51 +0300

    Course exercises

--- 12. SUBQUERIES AND JOINS - EXERCISE/Subqueries and Joins - Exercise.sql
+++ 12. SUBQUERIES AND JOINS - EXERCISE/Subqueries and Joins - Exercise.sql
@@ -195,8 +195,25 @@ GROUP BY c.country_code
 ORDER BY mountain_range DESC;
 
 #-- 14.	Countries with Rivers
+SELECT 
+    c.country_name, r.river_name
+FROM
+    countries AS c
+        LEFT JOIN
+    countries_rivers AS cr 
+		ON c.country_code = cr.country_code
+        LEFT JOIN
+    rivers AS r 
+		ON r.id = cr.river_id
+        JOIN
+    continents AS cn 
+		ON cn.continent_code = c.continent_code
+WHERE
+    cn.continent_name = 'Africa'
+ORDER BY c.country_name
+LIMIT 5;
 
-
+#-- 15.	*Continents and Currencies
 
 
 
