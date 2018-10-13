e19b6c47e19ecbeda474af0cfff6007328e33765
Bebrevski <stefan.bebrevski89@gmail.com>
2018-10-13 14:02:34 +0300

    Course exercises

--- 12. SUBQUERIES AND JOINS - EXERCISE/Subqueries and Joins - Exercise.sql
+++ 12. SUBQUERIES AND JOINS - EXERCISE/Subqueries and Joins - Exercise.sql
@@ -44,9 +44,51 @@ WHERE
 ORDER BY e.employee_id DESC;
 
 #-- 4.	Employee Departments
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.salary,
+    d.name AS 'department_name'
+FROM
+    employees AS e
+        JOIN
+    departments AS d 
+		ON e.department_id = d.department_id
+WHERE
+    e.salary > 15000
+ORDER BY d.department_id DESC
+LIMIT 5;
 
+#-- 5.	Employees Without Project
+SELECT 
+    e.employee_id, e.first_name
+FROM
+    employees AS e
+        LEFT JOIN
+    employees_projects AS ep 
+		ON ep.employee_id = e.employee_id
+WHERE
+    ep.employee_id IS NULL
+ORDER BY e.employee_id DESC
+LIMIT 3;
 
+#-- 6.	Employees Hired After
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    d.name AS 'dept_name'
+FROM
+    employees AS e
+        JOIN
+    departments AS d 
+		ON e.department_id = d.department_id
+WHERE
+        DATE(e.hire_date) > '1999-01-01'
+        AND d.name IN ('Sales' , 'Finance')
+ORDER BY hire_date ASC;
 
+#-- 7.	Employees with Project
 
 
 
