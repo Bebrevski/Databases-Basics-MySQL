543fde37958621d25bcdc217ecb23ec01c7bd682
Bebrevski <stefan.bebrevski89@gmail.com>
2018-10-13 15:26:47 +0300

    Course exercises

--- 12. SUBQUERIES AND JOINS - EXERCISE/Subqueries and Joins - Exercise.sql
+++ 12. SUBQUERIES AND JOINS - EXERCISE/Subqueries and Joins - Exercise.sql
@@ -89,6 +89,41 @@ WHERE
 ORDER BY hire_date ASC;
 
 #-- 7.	Employees with Project
+SELECT 
+    e.employee_id, e.first_name, p.name AS 'project_name'
+FROM
+    employees AS e
+        JOIN
+    employees_projects AS ep 
+		ON ep.employee_id = e.employee_id
+        JOIN
+    projects AS p 
+		ON ep.project_id = p.project_id
+WHERE
+    DATE(p.start_date) > '2002-08-13' 
+    AND p.end_date IS NULL
+ORDER BY e.first_name , p.name
+LIMIT 5;
+
+#-- 8.	Employee 24
+SELECT 
+    e.employee_id,
+    e.first_name,
+    IF(YEAR(p.start_date) >= '2005',
+        NULL,
+        p.name) AS 'project_name'
+FROM
+    employees AS e
+        JOIN
+    employees_projects AS ep ON e.employee_id = ep.employee_id
+        JOIN
+    projects AS p ON ep.project_id = p.project_id
+WHERE
+    e.employee_id = 24
+ORDER BY p.name;
+
+#-- 9.	Employee Manager
+
 
 
 
