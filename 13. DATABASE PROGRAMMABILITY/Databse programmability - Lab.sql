20f3580e59d563d7930cea3cf8845da8277f3d72
Bebrevski <stefan.bebrevski89@gmail.com>
2018-10-14 15:28:27 +0300

    Course exercises

--- 13. DATABASE PROGRAMMABILITY/Databse programmability - Lab.sql
+++ 13. DATABASE PROGRAMMABILITY/Databse programmability - Lab.sql
@@ -21,3 +21,60 @@ SELECT ufn_count_employees_by_town('Sofia');
 DROP FUNCTION IF EXISTS ufn_count_employees_by_town;
 
 #-- 2.	Employees Promotion
+DELIMITER $$
+CREATE PROCEDURE usp_raise_salaries(department_name VARCHAR(20))
+BEGIN
+	UPDATE employees AS e
+    JOIN departments AS d ON e.department_id = d.department_id
+    SET e.salary = e.salary * 1.05
+    WHERE d.name = department_name;
+END $$
+
+DELIMITER ;
+CALL usp_raise_salaries('Finance');
+
+#-- 3.	Employees Promotion By ID
+DELIMITER $$
+CREATE PROCEDURE usp_raise_salary_by_id(id INT(10))
+BEGIN
+	START TRANSACTION;
+    IF((SELECT COUNT(e.employee_id) 
+		FROM employees as e 
+		WHERE e.employee_id LIKE id)<>1) 
+		THEN ROLLBACK;
+    ELSE
+		UPDATE employees AS e
+		SET e.salary = e.salary * 1.05
+		WHERE e.employee_id = id;
+	END IF;
+END $$
+DELIMITER ;
+
+#-- 4.	Triggered
+CREATE TABLE deleted_employees(
+	employee_id INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+	middle_name VARCHAR(20),
+	job_title VARCHAR(50),
+	department_id INT,
+	salary DOUBLE 
+);
+
+DELIMITER $$
+CREATE TRIGGER tr_deleted_employees
+AFTER DELETE ON employees
+FOR EACH ROW
+BEGIN
+INSERT INTO deleted_employees
+    (first_name, last_name, middle_name, job_title, department_id, salary)
+    VALUES (OLD.first_name, OLD.last_name, 
+    OLD.middle_name, OLD.job_title, OLD.department_id, OLD.salary);
+END $$
+
+DELIMITER ;
+
+
+
+
+
